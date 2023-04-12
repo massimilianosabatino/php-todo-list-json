36d0cf65366220f71a8daf2b04d192f3b46087ce
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-04-12 18:00:34 +0200

    add: get data form back-end

---
+++ backend.php
@@ -0,0 +1,8 @@
+<?php
+$get_task_json = file_get_contents(__DIR__. '/task.json');
+$task = json_decode($get_task_json, true);
+
+// Send back info
+header('Content-Type: application/json');
+echo json_encode($task);
+?>
--- main.js
+++ main.js
@@ -2,16 +2,26 @@
 
 const { createApp } = Vue;
 
-const todos = JSON.parse('[{"text":"HTML","done":true},{"text":"CSS","done":true},{"text":"Responsive design","done":true},{"text":"Javascript","done":true},{"text":"PHP","done":true},{"text":"Laravel","done":false}]');
+
+// const todos = JSON.parse('[{"text":"HTML","done":true},{"text":"CSS","done":true},{"text":"Responsive design","done":true},{"text":"Javascript","done":true},{"text":"PHP","done":true},{"text":"Laravel","done":false}]');
 
 createApp({
     data(){
         return {
-            todos,
+            todos: [],
         }
     },
     methods: {
-
+        getTask(){
+            axios.post('backend.php', {
+                Headers: {'Content-Type' : 'multipart/form-data'}
+            }).then(response => {
+                this.todos = response.data;
+              })
+        }
         
+    },
+    created(){
+        this.getTask();
     }
 }).mount('#app')
